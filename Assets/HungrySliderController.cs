036da96e40db390f69f1fb6feb9bb5baf901d60d
d00074 <u83311705@gmail.com>
2019-01-26 14:43:30 +0900

    허기 수치랑 온도 수치

---
+++ Assets/HungrySliderController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HungrySliderController : MonoBehaviour
+{
+    public int hungry = 0;
+    public int maxHungry = 100;
+    Slider slider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider = gameObject.GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        slider.value = (float)hungry / maxHungry;
+    }
+}
---
+++ Assets/TemperatureSliderController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TemperatureSliderController : MonoBehaviour
+{
+    public int temperature = 0;
+    public int maxtemperature = 100;
+    Slider slider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider = gameObject.GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        slider.value = (float)temperature / maxtemperature;
+    }
+}
