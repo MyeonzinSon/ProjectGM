d399cd5e0d9d017719c30c4c91d411ad974c4fba
1102jake <jake11021102@gmail.com>
2019-01-26 14:39:22 +0900

    주인공 점프

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     public float speed = 1;
+    public float jumpForce = 100;
+    bool isOnGround;
+    Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        isOnGround = false;
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -21,5 +25,17 @@ public class NewBehaviourScript : MonoBehaviour
         if (Input.GetKey(KeyCode.RightArrow))
             transform.position = transform.position + new Vector3(0.1f * speed, 0, 0);
 
+        if (Input.GetKey(KeyCode.UpArrow) && isOnGround)
+        {
+            isOnGround = false;
+            rb.AddForce(new Vector2(0,jumpForce));
+            //transform.position = transform.position + new Vector3(0, 0.1f * speed, 0);
+        }
     }
-}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isOnGround = true;
+    }
+}  
+
