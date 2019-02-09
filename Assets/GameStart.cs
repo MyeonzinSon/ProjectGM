2efbfb1c121718e8ceef087fee68ba14fec40e45
d00074 <u83311705@gmail.com>
2019-02-09 16:37:48 +0900

    Ui 마지막 작업

--- Assets/GameStart.cs
+++ Assets/GameStart.cs
@@ -3,21 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class NextScene : MonoBehaviour
+public class GameStart : MonoBehaviour
 {
-    public void MoveScene()
-    {
-        SceneManager.LoadScene("Cutscene");
-    }
+    float timer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        if(timer > 6.6f)
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
     }
 }
