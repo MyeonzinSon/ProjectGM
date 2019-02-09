bbed20a5057d99dc6bf0789c867b9d283ba651e3
d00074 <u83311705@gmail.com>
2019-02-09 15:26:12 +0900

    hp, 허기와 온도가 0이됬을때, hp가 1칸씩 깍임

---
+++ Assets/HpSlider.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class HpSlider : MonoBehaviour
+{
+    public int HP = 0;
+    public int MaxHP = 5;
+    Slider slider;
+    public Image heart1, heart2, heart3, heart4, heart5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider = gameObject.GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        slider.value = (float)HP / MaxHP;
+
+        Color color;
+        color = gameObject.GetComponent<Image>().color;
+        color = new Color(1,0,0);
+        */
+        heart5.gameObject.SetActive(false);
+        if (HP == 5)
+        {
+            heart5.gameObject.SetActive(true);
+            heart4.gameObject.SetActive(true);
+            heart3.gameObject.SetActive(true);
+            heart2.gameObject.SetActive(true);
+            heart1.gameObject.SetActive(true);
+        }
+        else if (HP == 4)
+        {
+            heart5.gameObject.SetActive(false);
+            heart4.gameObject.SetActive(true);
+            heart3.gameObject.SetActive(true);
+            heart2.gameObject.SetActive(true);
+            heart1.gameObject.SetActive(true);
+        }
+        else if (HP == 3)
+        {
+            heart5.gameObject.SetActive(false);
+            heart4.gameObject.SetActive(false);
+            heart3.gameObject.SetActive(true);
+            heart2.gameObject.SetActive(true);
+            heart1.gameObject.SetActive(true);
+        }
+        else if (HP == 2)
+        {
+            heart5.gameObject.SetActive(false);
+            heart4.gameObject.SetActive(false);
+            heart3.gameObject.SetActive(false);
+            heart2.gameObject.SetActive(true);
+            heart1.gameObject.SetActive(true);
+        }
+        else if (HP == 1)
+        {
+            heart5.gameObject.SetActive(false);
+            heart4.gameObject.SetActive(false);
+            heart3.gameObject.SetActive(false);
+            heart2.gameObject.SetActive(false);
+            heart1.gameObject.SetActive(true);
+        }
+        else if (HP == 0)
+        {
+            heart5.gameObject.SetActive(false);
+            heart4.gameObject.SetActive(false);
+            heart3.gameObject.SetActive(false);
+            heart2.gameObject.SetActive(false);
+            heart1.gameObject.SetActive(false);
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+}
--- Assets/HungrySliderController.cs
+++ Assets/HungrySliderController.cs
@@ -6,18 +6,37 @@ using UnityEngine.UI;
 public class HungrySliderController : MonoBehaviour
 {
     public int hungry = 0;
+    public int hungryDecAmount = 4;
+    public float hungryDecTime = 10;
+    float timer;
     public int maxHungry = 100;
     Slider slider;
+    HpSlider hp;
 
     // Start is called before the first frame update
     void Start()
     {
         slider = gameObject.GetComponent<Slider>();
+        hp = FindObjectOfType<HpSlider>();
     }
 
     // Update is called once per frame
     void Update()
     {
         slider.value = (float)hungry / maxHungry;
+
+        timer = timer + Time.deltaTime;
+        if(timer >= hungryDecTime)
+        {
+            if (hungry > 0)
+            {
+                hungry = hungry - hungryDecAmount;
+            }
+            else
+            {
+                hp.HP -= 1;
+            }
+            timer = timer - hungryDecTime;
+        }
     }
 }
